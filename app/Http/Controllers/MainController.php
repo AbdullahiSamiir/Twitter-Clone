7a7c0c95578971a475af6ad6e1a62d1f9a93bd20
AbdullahiSamiir <abdullahisamiir44@gmail.com>
2024-03-17 17:18:54 +0300

    learned routing

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function feed()
+    {
+        return view('feed');
+    }
+
+    public function profile()
+    {
+        return view('users.profile');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [MainController::class, 'index']);
+Route::get('/feed', [MainController::class, 'feed']);
+Route::get('/profile', [MainController::class, 'profile']);
 
-Route::get('/', function () {
-    return view('index');
-});
 // Route::get('/', function () {
 //     return view('welcome');
 //     // return "<h1>Welcome to the new project</h1>";
